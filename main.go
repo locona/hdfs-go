7f783477a48c6a32b539f55c95ab4c1c5c0b9709
locona <locona.0810@gmail.com>
2018-08-20 19:21:03 +0900

    example

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"os"
+
+	"github.com/colinmarc/hdfs"
+	"github.com/k0kubun/pp"
+)
+
+func main() {
+	client, err := hdfs.New("localhost:8020")
+	err := client.Walk("/tmp", func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			pp.Println(info.Name())
+		}
+		return nil
+	})
+	//
+	// file, _ := client.Open("/mobydick.txt")
+	//
+	// buf := make([]byte, 59)
+	// file.ReadAt(buf, 48847)
+	//
+	// fmt.Println(string(buf))
+}
